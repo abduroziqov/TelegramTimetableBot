8ea59767530ca278c7d6d3631b56096bb6cced83
SignalMQ <matnazarovsobirjon123@gmail.com>
2025-02-02 11:57:53 +0500

    fix bug

--- TelegramTimetableBot/Program.cs
+++ TelegramTimetableBot/Program.cs
@@ -3,7 +3,6 @@ using TelegramTimetableBot;
 using TelegramTimetableBot.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddSingleton<IBrowser>();
 builder.Services.AddSingleton<TelegramBotService>();
 builder.Services.AddHostedService<Worker>();
 
