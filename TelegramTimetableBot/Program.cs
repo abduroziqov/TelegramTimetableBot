2255f79e5aafbb8fd8d080c9acf81c4267a33bfc
SignalMQ <matnazarovsobirjon123@gmail.com>
2025-02-02 11:59:57 +0500

    fix browser service bug

--- TelegramTimetableBot/Program.cs
+++ TelegramTimetableBot/Program.cs
@@ -3,6 +3,7 @@ using TelegramTimetableBot;
 using TelegramTimetableBot.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSingleton<BrowserService>();
 builder.Services.AddSingleton<TelegramBotService>();
 builder.Services.AddHostedService<Worker>();
 
